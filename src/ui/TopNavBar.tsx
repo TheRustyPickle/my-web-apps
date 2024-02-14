ec9b8a188a282dbc2f02168dd334371a2d03de59
Rusty Pickle <mohammad.sakib0194@gmail.com>
2024-02-15 01:56:22 +0600

    Remove useless icon on top

--- src/ui/TopNavBar.tsx
+++ src/ui/TopNavBar.tsx
@@ -15,58 +15,26 @@ const links = [
 const TopNavBar = () => {
 	const pathname = usePathname();
 	return (
-		<>
-			<div className="fixed top-0 left-0 right-0 z-50 bg-blue-500 p-3">
-				<div className="text-white text-lg antialiased mx-5 flex items-center">
-					<div className="hidden sm:block">
-						<a
-							href="https://github.com/TheRustyPickle"
-							target="_blank"
-							rel="noopener noreferrer"
+		<div className="fixed top-0 left-0 right-0 z-50 bg-blue-500 p-2 text-white text-lg antialiased">
+			<div className="flex space-x-4 grow justify-center">
+				{links.map((link) => {
+					return (
+						<Link
+							key={link.name}
+							href={link.href}
+							className={clsx(
+								" hover:bg-blue-600 hover:text-red-300 transition duration-300 px-3 py-1 rounded-lg",
+								{
+									"font-bold text-red-300": pathname === link.href,
+								},
+							)}
 						>
-							<Image
-								src="my-logo.svg"
-								alt="A green black R logo"
-								width="25"
-								height="20"
-							/>
-						</a>
-					</div>
-					<div className="flex space-x-4 grow justify-center">
-						{links.map((link) => {
-							return (
-								<Link
-									key={link.name}
-									href={link.href}
-									className={clsx(
-										" hover:bg-blue-600 hover:text-red-300 transition duration-300 px-3 py-1 rounded-lg",
-										{
-											"font-bold": pathname === link.href,
-										},
-									)}
-								>
-									<p>{link.name}</p>
-								</Link>
-							);
-						})}
-					</div>
-					<div className="hidden sm:block">
-						<a
-							href="https://github.com/TheRustyPickle/my-web-apps"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<Image
-								src="github-logo.svg"
-								alt="Github Logo"
-								width="35"
-								height="20"
-							/>
-						</a>
-					</div>
-				</div>
+							<p>{link.name}</p>
+						</Link>
+					);
+				})}
 			</div>
-		</>
+		</div>
 	);
 };
 
