204533457d378c64466a46d599a42216b03094bd
Rusty Pickle <mohammad.sakib0194@gmail.com>
2024-02-24 05:59:31 +0600

    Placeholder about page

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "About",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return <div className="absolute inset-x-0 top-20">{children}</div>;
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Home() {
+	return <h2 className="flex justify-center">Nothing Here yet!</h2>;
+}
